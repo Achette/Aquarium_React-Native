c849c09dee31a3171f6dfab9cb89b3dec733eceb
jpfalcuci <jpfalcuci@gmail.com>
2024-04-24 21:10:04 -0300

    style: text colors

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -2,4 +2,7 @@ export const Colors = {
   primary: '#0157A3',
   secondary: 'rgba(1, 87, 163, 0.1)',
   carouselText: '#898A8D',
+  subtitleText: '#0157A3',
+  placeholderText: '#898A8D',
+  inputErrorText: '#FF0000',
 };
