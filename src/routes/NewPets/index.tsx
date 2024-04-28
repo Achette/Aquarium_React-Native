fda3ac440969ba40a8217d5deabe4df6ca512585
jpfalcuci <jpfalcuci@gmail.com>
2024-04-28 12:05:03 -0300

    feat: accessories, pets e sensors

--- src/routes/NewPets/index.tsx
+++ src/routes/NewPets/index.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import { Text } from '@rneui/themed';
 import { Colors } from '../../theme/Colors';
 import { PrimaryButton, SecondaryButton } from '../../components/DefaultButtons';
+import { ItemButton } from '../../components/ItemButton';
 
 function NewPets({navigation}) {
+
+  const [hasFish, setHasFish] = useState(false);
+  const [hasTurtle, setHasTurtle] = useState(false);
+  const [hasSnake, setHasSnake] = useState(false);
+  const [hasFrog, setHasFrog] = useState(false);
+
+  const [fishQuantity, setFishQuantity] = useState(1);
+  const [turtleQuantity, setTurtleQuantity] = useState(1);
+  const [snakeQuantity, setSnakeQuantity] = useState(1);
+  const [frogQuantity, setFrogQuantity] = useState(1);
+
+  const fishIcon = require('../../assets/icons/fish.png');
+  const turtleIcon = require('../../assets/icons/turtle.png');
+  const snakeIcon = require('../../assets/icons/snake.png');
+  const frogIcon = require('../../assets/icons/frog.png');
+
+  const pets = [
+    { title: 'Peixe', icon: fishIcon, onPress: () => setHasFish(!hasFish), isSelected: hasFish, itemQuantity: fishQuantity, setQuantity: setFishQuantity},
+    { title: 'Tartaruga', icon: turtleIcon, onPress: () => setHasTurtle(!hasTurtle), isSelected: hasTurtle, itemQuantity: turtleQuantity, setQuantity: setTurtleQuantity},
+    { title: 'Cobra', icon: snakeIcon, onPress: () => setHasSnake(!hasSnake), isSelected: hasSnake, itemQuantity: snakeQuantity, setQuantity: setSnakeQuantity},
+    { title: 'Sapo', icon: frogIcon, onPress: () => setHasFrog(!hasFrog), isSelected: hasFrog, itemQuantity: frogQuantity, setQuantity: setFrogQuantity},
+  ]
 
   return (
     <ScrollView>
       <View style={styles.page}>
-        <Text style={[styles.title, {fontSize: 28}]}>Pets</Text>
+        <Text style={styles.title}>Pets</Text>
+
+        <View style={styles.petsButtonsContainer}>
+          {pets.map((item, index) => (
+            <ItemButton 
+              key={index}
+              icon={item.icon} 
+              title={item.title} 
+              onPress={item.onPress} 
+              isSelected={item.isSelected}
+              hasCounter={true}
+              itemQuantity={item.itemQuantity}
+              setQuantity={item.setQuantity}
+            />
+          ))}
+        </View>
 
         <View style={styles.buttonsContainer}>
           <PrimaryButton content="Avançar" onPress={() => navigation.navigate('Aquariums')} />
@@ -28,12 +66,21 @@ const styles = StyleSheet.create({
     margin: 24,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     color: Colors.primary,
     justifyContent: 'flex-start',
     alignSelf: 'flex-start',
-    marginVertical: 8,
+    marginTop: 8,
+    marginBottom: 16,
+  },
+  petsButtonsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    gap: 12,
+    marginVertical: 28,
+    paddingHorizontal: 10,
   },
   buttonsContainer: {
     width: '120%',
--- src/routes/NewSensors/index.tsx
+++ src/routes/NewSensors/index.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import { Text } from '@rneui/themed';
 import { Colors } from '../../theme/Colors';
 import { PrimaryButton, SecondaryButton } from '../../components/DefaultButtons';
+import { ItemButton } from '../../components/ItemButton';
 
 function NewSensors({navigation}) {
+
+  const [hasExternalTemperatureSensor, setHasExternalTemperatureSensor] = useState(false);
+  const [hasWaterLevelSensor, setHasWaterLevelSensor] = useState(false);
+  const [hasOxygenLevelSensor, setHasOxygenLevelSensor] = useState(false);
+  const [hasLuminositySensor, setHasLuminositySensor] = useState(false);
+  const [hasPhSensor, setHasPhSensor] = useState(false);
+  const [hasSaturationSensor, setHasSaturationSensor] = useState(false);
+  
+  const externalTemperatureIcon = require('../../assets/icons/temperaturesensor.png');
+  const waterLevelIcon = require('../../assets/icons/waterlevelsensor.png');
+  const oxygenLevelIcon = require('../../assets/icons/oxygensensor.png');
+  const luminosityIcon = require('../../assets/icons/luminositysensor.png');
+  const phIcon = require('../../assets/icons/phsensor.png');
+  const saturationIcon = require('../../assets/icons/saturationsensor.png');
+
+  const sensors = [
+    { title: 'Temperatura Externa', icon: externalTemperatureIcon, onPress: () => setHasExternalTemperatureSensor(!hasExternalTemperatureSensor), isSelected: hasExternalTemperatureSensor },
+    { title: 'Nível da Água', icon: waterLevelIcon, onPress: () => setHasWaterLevelSensor(!hasWaterLevelSensor), isSelected: hasWaterLevelSensor },
+    { title: 'Nível de Oxigênio', icon: oxygenLevelIcon, onPress: () => setHasOxygenLevelSensor(!hasOxygenLevelSensor), isSelected: hasOxygenLevelSensor },
+    { title: 'Luminosidade', icon: luminosityIcon, onPress: () => setHasLuminositySensor(!hasLuminositySensor), isSelected: hasLuminositySensor },
+    { title: 'pH', icon: phIcon, onPress: () => setHasPhSensor(!hasPhSensor), isSelected: hasPhSensor },
+    { title: 'Saturação', icon: saturationIcon, onPress: () => setHasSaturationSensor(!hasSaturationSensor), isSelected: hasSaturationSensor },
+  ]
 
   return (
     <ScrollView>
       <View style={styles.page}>
-        <Text style={[styles.title, {fontSize: 28}]}>Sensores</Text>
+        <Text style={styles.title}>Sensores</Text>
+
+        <View style={styles.sensorsButtonsContainer}>
+          {sensors.map((item, index) => (
+            <ItemButton 
+              key={index}
+              icon={item.icon} 
+              title={item.title} 
+              onPress={item.onPress} 
+              isSelected={item.isSelected}
+            />
+          ))}
+        </View>
 
         <View style={styles.buttonsContainer}>
           <PrimaryButton content="Avançar" onPress={() => navigation.navigate('NewPets')} />
@@ -28,12 +64,21 @@ const styles = StyleSheet.create({
     margin: 24,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     color: Colors.primary,
     justifyContent: 'flex-start',
     alignSelf: 'flex-start',
-    marginVertical: 8,
+    marginTop: 8,
+    marginBottom: 16,
+  },
+  sensorsButtonsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 12,
+    marginVertical: 28,
+  
   },
   buttonsContainer: {
     width: '120%',
